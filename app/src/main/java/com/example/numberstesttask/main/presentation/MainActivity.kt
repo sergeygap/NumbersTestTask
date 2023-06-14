64146909dfe25e7825de8ad39bd123480117c13e
sergeygap <1ggserongg1@gmail.com>
2023-06-14 21:33:05 +0300

    navigation and Fragment

--- app/src/main/java/com/example/numberstesttask/main/presentation/MainActivity.kt
+++ app/src/main/java/com/example/numberstesttask/main/presentation/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.numberstesttask.main.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.numberstesttask.R
 import com.example.numberstesttask.numbers.presentation.NumbersFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShowFragment {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, NumbersFragment())
-            .commit()
+        if (savedInstanceState == null)
+            show(NumbersFragment())
     }
+
+    private fun show(fragment: Fragment)
+    {
+        val transaction = supportFragmentManager.beginTransaction()
+        val container = R.id.container
+        transaction.replace(container, fragment)
+        transaction.commit()
+    }
+
+    override fun show(fragment: Fragment, something: Boolean) {
+
+    }
+
+
+}
+
+interface ShowFragment {
+    fun show(fragment: Fragment, something: Boolean)
 }
